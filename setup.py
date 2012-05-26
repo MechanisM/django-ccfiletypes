cc6883f84081b01a511e25383e567b9b5f0e6ac7
Jamie Curle <me@jamiecurle.com>
2012-05-26 14:13:33 +0100

    updated the readme

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from distutils.core import setup
 from ccthumbs import VERSION
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
-    name="django-ccthumbs",
+    name='django-ccfiletypes',
     version=VERSION,
     license = 'BSD 3 Clause',
-    description='A thumbnail generating model field for Django',
+    description='Django templatefilters for displaying info about files.',
+    long_description=long_description,
     author='c&c',
     author_email='studio@designcc.co.uk',
-    url='https://github.com/designcc/django-ccthumbs',
+    url='https://github.com/designcc/django-ccfiletypes',
     packages=[
-        'ccthumbs',
-        ],
-    install_requires = [
-        'PIL',
-        ])
+        'ccfiletypes',
+        ],)
 
